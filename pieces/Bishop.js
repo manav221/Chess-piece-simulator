7247d3552b751ad479e9fece676cb2af6c000ea3
manav221 <manavdeora221@gmail.com>
2026-07-23 16:14:12 +0530

    bishop all direction movement logic

--- pieces/Bishop.js
+++ pieces/Bishop.js
@@ -26,6 +26,63 @@ class Bishop extends Piece {
             }
         }
 
+        // down-left movement
+        for (let i = 1; i < 8; i++) {
+            let row = currentRow + i;
+            let col = currentCol - i;
+
+            if (row >= 8 || col < 0) break;
+
+            let piece = board.grid[row][col];
+
+            if (piece === null) {
+                possibleMoves.push([row, col]);
+            } else if (piece.color !== this.color) {
+                possibleMoves.push([row, col]);
+                break;
+            } else {
+                break;
+            }
+        }
+
+        // up-right
+        for (let i = 1; i < 8; i++) {
+            let row = currentRow - i;
+            let col = currentCol + i;
+
+            if (row < 0 || col >= 8) break;
+
+            let piece = board.grid[row][col];
+
+            if (piece === null) {
+                possibleMoves.push([row, col]);
+            } else if (piece.color !== this.color) {
+                possibleMoves.push([row, col]);
+                break;
+            } else {
+                break;
+            }
+        }
+
+        // up-left
+        for (let i = 1; i < 8; i++) {
+            let row = currentRow - i;
+            let col = currentCol - i;
+
+            if (row < 0 || col < 0) break;
+
+            let piece = board.grid[row][col];
+
+            if (piece === null) {
+                possibleMoves.push([row, col]);
+            } else if (piece.color !== this.color) {
+                possibleMoves.push([row, col]);
+                break;
+            } else {
+                break;
+            }
+        }
+
         return possibleMoves;
     }
 }
