4b31f3ef6466454cc6b80f7e7cf42dc8bf487bbd
manav221 <manavdeora221@gmail.com>
2026-07-24 18:29:41 +0530

    king movement logic

---
+++ pieces/King.js
@@ -0,0 +1,33 @@
+import Piece from "./piece.js";
+
+class King extends Piece {
+    constructor(color) {
+        super(color)
+    }
+
+    possibleMoves(board,currentRow, currentCol) {
+        const possibleMoves = [];
+        const offsets = [
+            [1, 0], [1, 1], [1, -1],
+            [-1, 0], [-1, 1], [-1, -1],
+            [0, 1], [0, -1]
+        ]
+
+        for (let i = 0; i < offsets.length; i++) {
+            let [r, c] = offsets[i];
+            let newRow = currentRow + r;
+            let newCol = currentCol + c;
+
+            if(newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8) {
+                let piece = board.getPiece(newRow,newCol);
+
+                if(piece === null) possibleMoves.push([newRow,newCol]);
+                else if(piece.color !== this.color) possibleMoves.push([newRow,newCol]);
+            };
+        }
+
+        return possibleMoves;
+    }
+}
+
+export default King;
